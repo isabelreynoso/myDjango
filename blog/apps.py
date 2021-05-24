6ec09c13e89dba493d5791d379b40ea76953483a
Isabel Reynoso <isabel.reynoso.21@bishops.com>
2021-05-24 11:43:11 -0700

    Addind Paths

--- blog/apps.py
+++ blog/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class BlogConfig(AppConfig):
     name = 'blog'
+    path = '/Users/Student/Desktop/django_project/blog'
--- users/apps.py
+++ users/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     name = 'users'
+    path = '/Users/Student/Desktop/django_project/users'
 
     def ready(self):
         import users.signals
